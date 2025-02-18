7bbf2b1e44e8781aa5a72fc1c53ea0f7ec81f3da
thundercookie15 <lucas.maijers@student.rocvf.nl>
2025-02-18 10:43:04 +0100

    clean imports

--- userinterface/main.py
+++ userinterface/main.py
@@ -15,7 +15,7 @@ import PySimpleGUI as sg
 import psutil
 import pydirectinput
 from irc.bot import SingleServerIRCBot
-from irc.client import Reactor, ServerConnection, ServerNotConnectedError, Event
+from irc.client import ServerConnection, ServerNotConnectedError, Event
 from irc.connection import Factory
 from jsonschema import ValidationError
 from pyuac import main_requires_admin
